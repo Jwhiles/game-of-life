78077ea1b71871e630d548b7ad3d65f51f54c840
John Whiles <johnwhiles@outlook.com>
2017-01-11 22:02:07 +0000

    set an interval so the NEVER STOPS

--- index.js
+++ index.js
@@ -116,5 +116,7 @@ let state = turn(['1:1','2:2','3:3','1:2','4:4','3:4','5:4','6:4'])
 state = doit(state)
 
 document.getElementsByClassName('start')[0].addEventListener('click', () => {
-  state = doit(state)
+  setInterval(function(){
+    state = doit(state)
+  }, 225);
 })
