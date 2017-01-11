e11eec503b5a6b86fb04bf89bd5134feb0bcbedd
John Whiles <johnwhiles@outlook.com>
2017-01-11 20:16:14 +0000

    make the visualiser cope with values that go off the grid

--- visualise.js
+++ visualise.js
@@ -14,10 +14,15 @@ const changeMatrix = (matrix, coord) => {
   const [xS, yS] = coord.split(':');
   const x = Number(xS)
   const y = Number(yS)
+  const dimensions = matrix.length;
+  if (x < matrix.length && x >= 0 && y < matrix.length && y >= 0) {
+  console.log(x, y);
   const newTrix = matrix.slice(0);
-  console.log(newTrix);
   newTrix[y][x] = 1;
   return newTrix;
+  } else {
+  return matrix;
+  }
 }
 
 const interpretGameState = (matrix, game) => {
@@ -27,7 +32,7 @@ const interpretGameState = (matrix, game) => {
   })
   return newTrix
 }
-// 
+//
 // const init = ['0:1','0:2','0:3','1:1','1:3','2:1','2:2','2:3'];
 //
 // const state = interpretGameState(makeGrid(5), init);
